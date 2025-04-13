1ae5ed1065c96b3dc43d34bfea7b6faab585ac6c
MRCX-Personal <86763231+MRCX-Personal@users.noreply.github.com>
2025-04-13 21:47:06 +0800

    Add files via upload

---
+++ topic/4/纪念抗日战争胜利七十周年 - 哔哩哔哩 (゜-゜)つロ 干杯~-bilibili_files/core.php
@@ -0,0 +1,39 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+!function(){var p,q,r,a=function(){var b,c,d,e,a=document.getElementsByTagName("script");for(b=0,c=a.length;c>b;b++)if(e=a[b],e.src&&(d=/^(https?:)\/\/[\w\.\-]+\.cnzz\.com\//i.exec(e.src)))return d[1];return window.location.protocol}(),b=encodeURIComponent,c="2724999",d="",e="",f="online_v3.php",g="hzs11.cnzz.com",h="1",i="text",j="z",k="&#31449;&#38271;&#32479;&#35745;",l=window["_CNZZDbridge_"+c]["bobject"],m="0",n=a+"//web.archive.org/web/20190713095028/https://online.cnzz.com/online/"+f,o=[];o.push("id="+c),o.push("h="+g),o.push("on="+b(e)),o.push("s="+b(d)),n+="?"+o.join("&"),"0"===m&&l["callRequest"]([a+"//web.archive.org/web/20190713095028/https://cnzz.mmstat.com/9.gif?abc=1"]),h&&(""!==e?l["createScriptIcon"](n,"utf-8"):(q="z"==j?"https://web.archive.org/web/20190713095028/https://www.cnzz.com/stat/website.php?web_id="+c:"https://web.archive.org/web/20190713095028/https://quanjing.cnzz.com","pic"===i?(r=a+"//web.archive.org/web/20190713095028/https://icon.cnzz.com/img/"+d+".gif",p="<a href='"+q+"' target=_blank title='"+k+"'><img border=0 hspace=0 vspace=0 src='"+r+"'></a>"):p="<a href='"+q+"' target=_blank title='"+k+"'>"+k+"</a>",l["createIcon"]([p])))}();(function(){var u=function(){for(var a=document.getElementsByTagName("script"),b=0,d=a.length;b<d;b++){var f,e=a[b];if(e.src&&(f=/^(https?:)\/\/[\w\.\-]+\.cnzz\.com\//i.exec(e.src)))return f[1]}return window.location.protocol}();try{var m="https://web.archive.org/web/20190713095028/http://hm3.cnzz.com/",q=['https://web.archive.org/web/20190713095028/http://www.bilibili.tv/'],g=document,f=window,n=encodeURIComponent,r="unknow",l=null,t=function(){this.c()};t.prototype={c:function(){if(!1===this.f())return!1;this.a(g,"mousedown",this.b);var a=f.navigator.userAgent;
+l=g.documentElement&&0!==g.documentElement.clientHeight?g.documentElement:g.body;a=a?a.toLowerCase().replace(/-/g,""):"";for(var b="netscape;se 1.;se 2.;saayaa;360se;tencent;qqbrowser;mqqbrowser;maxthon;myie;theworld;konqueror;firefox;chrome;safari;msie 5.0;msie 5.5;msie 6.0;msie 7.0;msie 8.0;msie 9.0;msie 10.0;Mozilla;opera".split(";"),d=0;d<b.length;d+=1)if(-1!==a.indexOf(b[d])){r=b[d];break}},a:function(a,b,d){a.addEventListener?a.addEventListener(b,d,!1):a.attachEvent?a.attachEvent("on"+b,d):
+a["on"+b]=d},b:function(a){a||(a=f[a]);var b=a.target||a.srcElement;"IMG"===b.tagName&&(b=b.parentNode);b="A"===b.tagName?1:0;var d=a.which||a.button,p=a.clientX;a=a.clientY;var e=f.pageYOffset||l.scrollTop;p+=f.pageXOffset||l.scrollLeft;a+=e;e=l.clientWidth||f.innerWidth;var k=f.location.href,c=[];c.push("id=2724999");c.push("x="+p);c.push("y="+a);c.push("w="+e);c.push("s="+f.screen.width+"x"+f.screen.height);c.push("b="+r);c.push("c="+d);c.push("r="+n(g.referrer));c.push("a="+b);""!==
+f.location.hash&&c.push("p="+n(k));c.push("random="+n(Date()));b=c.join("&");var h=new Image;h.onload=h.onerror=h.onabort=function(){h=h.onload=h.onerror=h.onabort=null};"https:"===u&&(m=m.replace(/^http:/,"https:"));h.src=m+"heatmap.gif?"+b;return!0},f:function(){var a=f.location.href,b=!1,d="([{\\^$|)?+.]}".split("");f.location.pathname||(a+="/");for(var g=0;g<q.length;g++){var e=q[g];if(-1!==e.indexOf("*")){for(var k=0;k<d.length;k++){var c="/\\"+d[k]+"/g";e=e.replace(eval(c),"\\"+d[k])}c="/\\*/g";
+e=e.replace(eval(c),"(.*)");c=RegExp(e,"i");if(c.test(a)){b=!0;break}}else if(e===a){b=!0;break}}return b}};new t}catch(a){}})();
+
+}
+/*
+     FILE ARCHIVED ON 09:50:28 Jul 13, 2019 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 12:59:59 Apr 13, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.502
+  exclusion.robots: 0.02
+  exclusion.robots.policy: 0.009
+  esindex: 0.009
+  cdx.remote: 10.814
+  LoadShardBlock: 325.631 (3)
+  PetaboxLoader3.datanode: 167.474 (4)
+  PetaboxLoader3.resolve: 244.852 (2)
+  load_resource: 139.27
+*/
